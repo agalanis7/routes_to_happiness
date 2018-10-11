52b1e8c3547e24ad7d1e487b4c908385d1d4ca1d
agalanis7 <agalanis7@gmail.com>
2018-10-11 15:46:08 -0400

    homework

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './CSS/index.css';
 
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
+
+
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <div>
+        <p onClick={() => this.props.history.push('/')}>Home</p>
+        <p onClick={() => this.props.history.push('/Happy')}>Happy</p>
+        <p onClick={() => this.props.history.push('/Sleepy')}>Sleepy</p>
+        <p onClick={() => this.props.history.push('/Guilty')}>Guilty</p>
+      </div>
     )
   }
 }
 
-ReactDOM.render( <Home />, document.getElementById('root') );
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={Home} />
+        <Route path="/Happy" component={ Happy } />
+        <Route path="/Sleepy" component={ Sleepy } />
+        <Route path="/Guilty" component={ Guilty } />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render( <Root />, document.getElementById('root') );
